4f546800f6f374263c9104d6ba49934dd7d42f06
elhadjimalick-dione <146718842+elhadjimalick-dione@users.noreply.github.com>
2024-01-25 17:03:06 +0100

    Push add pokedex pokemon

--- pokedex.py
+++ pokedex.py
@@ -1,46 +1,73 @@
+import pygame
 import json
-from pokemon import *
+
 class Pokedex:
+    
     def __init__(self):
-        self.encountered_pokemon = []
-
-    def add_pokemon(self, name):
-        # Vérification des doublons avant d'ajouter le Pokémon
-        if name not in self.encountered_pokemon:
-            self.encountered_pokemon.append(name)
-            print(f"{name} a été ajouté au Pokedex.")
-        else:
-            print(f"{name} est déjà dans le Pokedex.")
         
-    def display_all_pokemon(self):
-        # Affiche tous les Pokémon du Pokedex
-        print("Pokémon rencontrés :")
-        for pokemon in self.encountered_pokemon:
-            print(pokemon)
-        print(f"Nombre total de Pokémon rencontrés : {len(self.encountered_pokemon)}")
+        self.Data = []
+        with open ('donnees_pokemon.json', 'r' ) as f:
+            self.Data=json.load(f)
+
+
         
-    def save_to_json(self):
-        # Sauvegarde les données du Pokedex dans un fichier JSON
-        with open('pokedex.json', 'w') as json_file:
-            json.dump(self.encountered_pokemon, json_file)
-            print("Pokedex sauvegardé avec succès")
 
-    def load_from_json(self):
-        # Charge les données du fichier JSON dans le Pokedex
-        try:
-            with open('pokedex.json', 'r') as json_file:
-                data = json.load(json_file)
-                self.encountered_pokemon = data
-                print("Pokedex chargé avec succès.")
-        except FileNotFoundError:
-            print("Aucun fichier Pokedex trouvé.")
+    def creation_json(self):
+        
+        self.pokedex = []
+        
+        with open('pokedex.json','w') as f:
+            json.dump(self.pokedex,f)
 
-# Exemple d'utilisation
+    
+    #les 3 premiers pokemon de debut de jeu dans le pokedex
+    def pokemon_debut(self):
+        
+        for i in range(3):
+            self.pokemon = self.Data[f'pokemon{i+1}']
+            with open('pokedex.json','r+') as f:
+                self.pokedex=json.load(f)
+                self.pokedex.append(self.pokemon)
+                f.seek(0)
+                print(i)
+                json.dump(self.pokedex,f,indent=2)
+        
+        
+    
+        
+        
+          
 pokedex = Pokedex()
-pokedex.add_pokemon("Salameche")
-pokedex.add_pokemon("Carapuce")
-# Sauvegarde du Pokedex dans un fichier JSON
-pokedex.save_to_json()
-# Chargement du Pokedex depuis le fichier JSON
-pokedex.load_from_json()
-pokedex.display_all_pokemon()
+pokedex.creation_json()
+pokedex.pokemon_debut()
+
+
+
+
+  
+
+    # def save_to_json(self):
+    #     with open('pokedex.json', 'w') as json_file:
+    #         json.dump(self.pokedex, json_file, indent=2)
+    #         print("Pokedex sauvegardé avec succès")
+
+    # def load_from_json(self):
+    #     try:
+    #         with open('pokedex.json', 'r') as json_file:
+    #             self.pokedex = json.load(json_file)
+    #             print("Pokedex chargé avec succès.")
+    #     except FileNotFoundError:
+    #         print("Aucun fichier Pokedex trouvé.")
+    
+    # def get_pokemon_for_combat(self):
+    #     return self.pokedex
+
+
+
+
+
+# # Sauvegarde du Pokedex dans un fichier JSON
+# pokedex.save_to_json()
+
+# # Chargement du Pokedex depuis le fichier JSON
+# pokedex.load_from_json()
