a589b16169eb2045e5a671550ebba3577f718ce9
mounir-merzoud <mounir.merzoud@laplateforme.io>
2024-02-14 10:43:23 +0100

    faire un commit pour la branch

--- class/pokedex.py
+++ class/pokedex.py
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import json
-from pokemon import *
-class Pokedex:
-    def __init__(self):
-        self.encountered_pokemon = []
-
-    def add_pokemon(self, name):
-        # Vérification des doublons avant d'ajouter le Pokémon
-        if name not in self.encountered_pokemon:
-            self.encountered_pokemon.append(name)
-            print(f"{name} a été ajouté au Pokedex.")
-        else:
-            print(f"{name} est déjà dans le Pokedex.")
-        
-    def display_all_pokemon(self):
-        # Affiche tous les Pokémon du Pokedex
-        print("Pokémon rencontrés :")
-        for pokemon in self.encountered_pokemon:
-            print(pokemon)
-        print(f"Nombre total de Pokémon rencontrés : {len(self.encountered_pokemon)}")
-        
-    def save_to_json(self):
-        # Sauvegarde les données du Pokedex dans un fichier JSON
-        with open('pokedex.json', 'w') as json_file:
-            json.dump(self.encountered_pokemon, json_file)
-            print("Pokedex sauvegardé avec succès")
-
-    def load_from_json(self):
-        # Charge les données du fichier JSON dans le Pokedex
-        try:
-            with open('pokedex.json', 'r') as json_file:
-                data = json.load(json_file)
-                self.encountered_pokemon = data
-                print("Pokedex chargé avec succès.")
-        except FileNotFoundError:
-            print("Aucun fichier Pokedex trouvé.")
-
-# Exemple d'utilisation
-pokedex = Pokedex()
-pokedex.add_pokemon("Salameche")
-pokedex.add_pokemon("Carapuce")
-# Sauvegarde du Pokedex dans un fichier JSON
-pokedex.save_to_json()
-# Chargement du Pokedex depuis le fichier JSON
-pokedex.load_from_json()
-pokedex.display_all_pokemon()
-=======
 import pygame
 import json
 import os
@@ -180,4 +132,3 @@ if __name__ == "__main__":
     pokedex_manager.enregistrer_pokemon_perdant("pokemon_perdant")
     pokedex = Pokedex("pokedex.json", 600)
     pokedex.run()
->>>>>>> main
