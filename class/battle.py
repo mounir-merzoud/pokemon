f61445ac51e4b28a8eb3ab1bc823cf65e68b0e7d
mounir-merzoud <mounir.merzoud@laplateforme.io>
2024-02-14 11:32:45 +0100

    merger la branch main

--- class/battle.py
+++ class/battle.py
@@ -6,33 +6,7 @@ import random
 from barre_de_vie import BarreDeVie
 from pokemon import Pokemon
 from choix_de_pokemon import Menu
-
-class Pokedex:
-    def __init__(self, fichier_pokedex):
-        self.fichier_pokedex = fichier_pokedex
-
-    def charger_pokedex(self):
-        try:
-            with open(self.fichier_pokedex, "r") as fichier:
-                data = fichier.read()
-                if data:
-                    return json.loads(data)
-                else:
-                    print("Le fichier pokedex.json est vide.")
-                    return []
-        except FileNotFoundError:
-            print("Le fichier pokedex.json n'existe pas.")
-            return []
-        except json.decoder.JSONDecodeError:
-            print("Le fichier pokedex.json contient des données invalides.")
-            return []
-
-    def enregistrer_pokemon_perdant(self, pokemon_perdant):
-        pokedex = self.charger_pokedex()
-        pokedex.append(pokemon_perdant)
-        with open(self.fichier_pokedex, "w") as fichier:
-            json.dump(pokedex, fichier)
-
+from pokedex import *
 class Battle:
     def __init__(self):
         pygame.init()
